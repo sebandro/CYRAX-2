76d027c1bc9bb0983658fc65ea4996de9c333804
sebandro <alejandrosebastian654@gmail.com>
2026-03-19 19:59:43 -0300

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,35 @@
+const products = [
+
+
+{
+id:1,
+name:"Short Active Black",
+price:21990,
+category:"shorts",
+featured:false,
+images:[
+"assets/img/short1.jpg",
+"assets/img/morena.jpg",
+"assets/img/rubia.jpg",
+"assets/img/prod1.jpg",
+
+]
+},
+
+{
+id:2,
+name:"Calza Fit Black",
+price:29990,
+category:"calzas",
+featured:true,
+images:[
+"assets/img/rubia.jpg",
+"assets/img/morena.jpg",
+]
+},
+
+
+];
+
+
+
